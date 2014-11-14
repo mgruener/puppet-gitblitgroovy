ab746fc29c9f5a22425156b4cdaed0246f4537cb
Michael Gruener <michael.gruener@chaosmoon.net>
2014-11-15 00:29:37 +0100

    puppetchecks: Add dependencies and restructure a little bit

--- manifests/puppetchecks.pp
+++ manifests/puppetchecks.pp
@@ -3,18 +3,19 @@ class gitblitgroovy::puppetchecks (
 ) {
   require gitblitgroovy
 
+  $source = "http://central.maven.org/maven2/org/eclipse/jgit/org.eclipse.jgit.archive/${jgitversion}"
+  $file = "org.eclipse.jgit.archive-${jgitversion}.jar"
+  staging::file { $file:
+    source => "${source}/${file}",
+    target => "${::gitblit::installdir}/ext/${file}",
+  }
+
   file { "${::gitblit::datadir}/groovy/puppet-checks.groovy":
     ensure  => present,
     content => file('gitblitgroovy/gitblit-groovy/puppet-checks.groovy'),
     owner   => $::gitblit::user,
     group   => $::gitblit::group,
     mode    => '0644',
-  }
-
-  $source = "http://central.maven.org/maven2/org/eclipse/jgit/org.eclipse.jgit.archive/${jgitversion}"
-  $file = "org.eclipse.jgit.archive-${jgitversion}.jar"
-  staging::file { $file:
-    source => "${source}/${file}",
-    target => "${::gitblit::installdir}/ext/${file}",
+    require => Staging::File[$file],
   }
 }
