d934d8e252589da9162e6bfe4e8d4b4970b5e5a1
Michael Gruener <michael.gruener@chaosmoon.net>
2014-11-15 00:13:30 +0100

    puppetchecks: Add support for downloading the jgit archive jar

--- manifests/puppetchecks.pp
+++ manifests/puppetchecks.pp
@@ -1,4 +1,6 @@
-class gitblitgroovy::puppetchecks {
+class gitblitgroovy::puppetchecks (
+  $jgitversion = '3.5.1.201410131835-r'
+) {
   require gitblitgroovy
 
   file { "${::gitblit::datadir}/groovy/puppet-checks.groovy":
@@ -7,5 +9,12 @@ class gitblitgroovy::puppetchecks {
     owner   => $::gitblit::user,
     group   => $::gitblit::group,
     mode    => '0644',
+  }
+
+  $source = "http://central.maven.org/maven2/org/eclipse/jgit/org.eclipse.jgit.archive/${jgitversion}"
+  $file = "org.eclipse.jgit.archive-${jgitversion}.jar"
+  staging::file { $file:
+    source => "${source}/${file}",
+    target => "${::gitblit::installdir}/ext",
   }
 }
