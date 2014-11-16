01384dcd1f91efb80b2828ff58db61c911e70a2b
Michael Gruener <michael.gruener@chaosmoon.net>
2014-11-16 11:42:17 +0100

    Add r10kdeploy class
    The actual script it should be deploying will follow at
    a later time. The class is neede right now to install
    sudo rules

---
+++ manifests/r10kdeploy.pp
@@ -0,0 +1,26 @@
+class gitblitgroovy::r10kdeploy (
+  $use_sudo = true,
+  $runas    = 'puppet',
+) {
+  require gitblitgroovy
+
+  if $use_sudo {
+    sudo::sudoers { 'r10kdeploy':
+      ensure  => 'present',
+      comment => 'Allow r10k to be called by gitblit',
+      users   => ['gitblit'],
+      runas   => [$runas],
+      cmnds   => ['r10k'],
+      tags    => ['NOPASSWD'],
+    }
+  }
+
+#  file { "${::gitblit::datadir}/groovy/r10k-deploy.groovy":
+#    ensure  => present,
+#    content => file('gitblitgroovy/gitblit-groovy/r10k-deploy.groovy'),
+#    owner   => $::gitblit::user,
+#    group   => $::gitblit::group,
+#    mode    => '0644',
+#    require => Staging::File[$file],
+#  }
+}
